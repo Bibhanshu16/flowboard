a65bd913c2eaf004d7817fdc23fa7d365c3b680b
Bibhanshu16 <bibhanshus394@gmail.com>
2025-12-09 08:35:12 +0530

    Initial commit from Create Next App

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+
+
+export const COLUMN_IDS = ['todo', 'in-progress', 'done'];
+
+export const INITIAL_COLUMNS = {
+  'todo': { id: 'todo', title: 'To Do', tasks: [] },
+  'in-progress': { id: 'in-progress', title: 'In Progress', tasks: [] },
+  'done': { id: 'done', title: 'Done', tasks: [] }
+};
